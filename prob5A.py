1a3c5a285935378b67ff86f9ac980890146dc6ba
ADITYA DOBHAL <123196030+Dctrzadi@users.noreply.github.com>
2026-07-29 23:52:44 +0530

    Add files via upload

---
+++ prob5A.py
@@ -0,0 +1,8 @@
+# Write a program to create a dictionary of English words with values as their Hindi translation. Provide user with an option to look it up!
+words = {
+    "Help":'madad',
+    "Chair":'khursi',
+    "Cat":'billi',
+}
+word = input("Enter the word you want to search in dictionary:-")
+print(words[word])
---
+++ prob5C.py
@@ -0,0 +1,4 @@
+#Can we have a set with 18(int) and '18'(str) as a value in it 
+set={18,'18'}
+print(set)
+#It will give 2 different 18 one as int and one as string
